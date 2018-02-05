380ba568f8c1d8b5d39debdbb851d3a0d6a174ca
DaeunGod <dcj4655@naver.com>
2018-02-05 12:42:33 +0900

    Add sum method

--- Calculator.py
+++ Calculator.py
@@ -1,3 +1,12 @@
 class Calculator():
     """docstring for Calculator."""
-    pass
+    def __init__(self):
+        pass
+
+    def sum(arg1, arg2):
+        return arg1 + arg2
+
+a = 4
+b = 2
+calc = Calculator()
+print(calc.sum(a, b))
